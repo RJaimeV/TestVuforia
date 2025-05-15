bff0785efa46ece2e5366544fb4de5cf04325859
RJaimeV <156475750+RJaimeV@users.noreply.github.com>
2025-05-14 20:36:46 -0600

    Add Dance

---
+++ Assets/Scripts/InstantiateObject.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToInstantiate;
+    public void Instantiate(Transform target)
+    {
+     Instantiate(objectToInstantiate, target.position, Quaternion.identity);
+    }
+}
