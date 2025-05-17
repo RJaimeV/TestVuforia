f98f54487dc1e376e5c89c54518cefb76e738823
RJaimeV <156475750+RJaimeV@users.noreply.github.com>
2025-05-16 18:36:31 -0600

    Buttons

---
+++ Assets/Scripts/DanceController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DanceController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _onSelectDance;
+    [SerializeField]
+    private UnityEvent _onDanceSelected;
+    public void ActivateSelectDance()
+    {
+        _onSelectDance?.Invoke();
+    }
+    public void OnSelectedDance()
+    {
+        _onDanceSelected?.Invoke();
+    }
+}
