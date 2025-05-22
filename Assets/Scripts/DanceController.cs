e2e677ec9a3f8328916388eba3ad3b5c53b22f41
RJaimeV <156475750+RJaimeV@users.noreply.github.com>
2025-05-21 20:34:37 -0600

    Timer Added

--- Assets/Scripts/DanceController.cs
+++ Assets/Scripts/DanceController.cs
@@ -4,15 +4,24 @@ using UnityEngine.Events;
 public class DanceController : MonoBehaviour
 {
     [SerializeField]
+    private Animator _characterAnimator;
+    [SerializeField]
     private UnityEvent _onSelectDance;
     [SerializeField]
     private UnityEvent _onDanceSelected;
+    private SoundData _currentSoundData;
     public void ActivateSelectDance()
     {
         _onSelectDance?.Invoke();
     }
-    public void OnSelectedDance()
+    public void OnSelectedDance(SoundData soundData)
     {
+        _currentSoundData = soundData;
         _onDanceSelected?.Invoke();
+    }
+    public void StartDance()
+    {
+        _characterAnimator.Play(_currentSoundData.animationName);
+        SoundManager.instance.PlayMusic(_currentSoundData.musicName);
     }
 }
